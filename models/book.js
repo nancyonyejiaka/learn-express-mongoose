a8b3545c17b8d150a3fd4b3b0419693312bc59ff
Nancy Onyejiaka <nancyonyejiaka@Nancys-MacBook-Pro.local>
2024-03-15 17:10:24 -0400

    03/15: class activity

--- models/book.js
+++ models/book.js
@@ -1,10 +1,14 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
-var BookSchema = new Schema(
-  {}
-);
+var BookSchema = new Schema({
+  title: { type: String, require: true },
+  author: { type: Schema.Types.ObjectId, ref: "Author", require: true },
+  summary: { type: String, require: true },
+  isbn: { type: String, require: true },
+  genre: [{ type: Schema.Types.ObjectId, ref: "Genre" }],
+});
 
 //Export model
-module.exports = mongoose.model('Book', BookSchema);
+module.exports = mongoose.model("Book", BookSchema);
--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,15 @@
-let BookInstance = require('../models/bookinstance');
+let BookInstance = require("../models/bookinstance");
 
-exports.show_all_books_status = function(res) {
-  return res.send([]);
-}
+exports.show_all_books_status = function (res) {
+  BookInstance.find({ status: { $eq: "Available" } })
+    .populate("book")
+    .exec()
+    .then((bookInstance_list) => {
+      res.send(
+        bookInstance_list.map(function (bookInstance) {
+          return bookInstance.book.title + " : " + bookInstance.status;
+        })
+      );
+    })
+    .catch((err) => res.send('Book status not found'));
+};
